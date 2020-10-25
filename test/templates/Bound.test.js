1fcdc875d69fe252d9e865de1d0183420a472b91
Kane Baccigalupi <baccigalupi@gmail.com>
2020-10-25 15:21:38 -0700

    Working bound templates

---
+++ test/templates/Bound.test.js
@@ -0,0 +1,75 @@
+import jsx from '../../lib/jsx'
+import { bind } from '../../lib/templates/Bound'
+
+import { assert } from 'chai'
+
+import { createTestDom, domToString } from '../support/testDom'
+
+describe('Bound Templates', () => {
+  it('renders basic html via jsx', () => {
+    const Template = ({name}) => <h1>Hello {name}!</h1>
+    const viewModel = (state) => {
+      return {
+        name: state.user.firstName 
+      }
+    }
+
+    const BoundTemplate = bind(Template, viewModel)
+    const template = <BoundTemplate />
+
+    const document = createTestDom()
+    const state = { user: { firstName: 'Kane', lastName: 'Baccigalupi' } }
+    const node = template.render({ document, state })
+
+    assert.equal(domToString(node), '<h1>Hello Kane!</h1>')
+  })
+
+  it('works with attributes', () => {
+    const Template = ({name, greeting}) => <h1>{greeting} {name}!</h1>
+    const viewModel = (state) => {
+      return {
+        name: state.user.firstName 
+      }
+    }
+
+    const BoundTemplate = bind(Template, viewModel)
+    const template = <BoundTemplate greeting='Hola' />
+
+    const document = createTestDom()
+    const state = { user: { firstName: 'Kane', lastName: 'Baccigalupi' } }
+    const node = template.render({ document, state })
+
+    assert.equal(domToString(node), '<h1>Hola Kane!</h1>')
+  })
+
+  it('works with children', () => {
+    const Template = ({name, greeting, children}) => (
+      <div>
+        <h1>{greeting} {name}!</h1>
+        { children }
+      </div>
+    )
+    const viewModel = (state) => {
+      return {
+        name: state.user.firstName
+      }
+    }
+
+    const BoundTemplate = bind(Template, viewModel)
+    const template = (
+      <BoundTemplate greeting='Hola'>
+        <p>We are pleased to have a member of the family.</p>
+      </BoundTemplate>
+    )
+
+
+    const document = createTestDom()
+    const state = { user: { firstName: 'Kane', lastName: 'Baccigalupi' } }
+    const node = template.render({ document, state })
+
+    assert.equal(
+      domToString(node), 
+      '<div><h1>Hola Kane!</h1><p>We are pleased to have a member of the family.</p></div>'
+    )
+  })
+})
