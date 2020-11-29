2031c3c88fb38a2fec379876c4aa2da985ec007b
Kane Baccigalupi <baccigalupi@gmail.com>
2020-11-28 21:41:35 -0800

    Integration specs

---
+++ cypress/integration/rendering.spec.js
@@ -0,0 +1,31 @@
+/// <reference types="cypress" />
+
+context('First time rendering and routes', () => {
+  it('renders the home page', () => {
+    cy.visit('http://localhost:1234')
+      .get('h1')
+      .should('contain', 'Hey now, hey now!')
+  })
+
+  it('uses location state to change the dom', () => {
+    cy.visit('http://localhost:1234')
+      .get('.nav-tabs .nav-link')
+      .first()
+      .should('have.class', 'active')
+  })
+
+  it('renders other pages with bound location state', () => {
+    cy.visit('http://localhost:1234/counter')
+      .get('h1')
+      .should('contain', 'Let\'s increment something')
+
+    cy.get('.nav-tabs .nav-link.active')
+      .should('contain', 'State change')
+  })
+
+  it('uses fuzzy route matching', () => {
+    cy.visit('http://localhost:1234/users/12')
+      .get('h1')
+      .should('contain', 'User details')
+  })
+})
